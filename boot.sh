56e893b835a0e8fa45f363ee17d04ed5ed8220a6
Isa Aguilar <isa@aguilarstory.com>
2019-04-14 22:03:37 -0700

    tail logs from web-server

--- boot.sh
+++ boot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-gunicorn -D -b :5001 --access-logfile - --error-logfile - --certfile=${CERT:-/tls/cert} --keyfile=${KEY:-/tls/key} admissions-webhook:app
-gunicorn -D -b :5000 --access-logfile - --error-logfile - admissions-webhook:app
-tail -f /var/log/faillog
-
+gunicorn -D -b :5001 --access-logfile app.log --error-logfile app.log --certfile=${CERT:-/tls/cert} --keyfile=${KEY:-/tls/key} admissions-webhook:app
+gunicorn -D -b :5000 --access-logfile app.log --error-logfile app.log admissions-webhook:app
+tail -f app.log
